60c616458744f1ea4c777e0ce16f606b7f7e7063
Ruslan Agaverdiev <rusl21ak@gmail.com>
2020-08-16 18:56:28 +0300

    JS HW1

---
+++ task-2.js
@@ -0,0 +1,16 @@
+const invoice = 50; 
+const stock = 100;
+
+// Write code under this line
+let message = 'Заказ оформлен, с вами свяжется менеджер'
+     if (invoice <= stock) 
+     
+     'message = ${message};'
+	
+      else 
+     message = 'На складе недостаточно товаров!'
+     'message = ${message}'
+	
+;
+
+console.log(message);
